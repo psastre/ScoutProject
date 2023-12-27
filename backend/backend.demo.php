47d668306db789f1394508b09ec70f070d3bd56b
Pedro Sastre <pedro2000sastre@gmail.com>
2023-12-27 20:17:28 -0300

    query por games

--- backend/backend.demo.php
+++ backend/backend.demo.php
@@ -19,3 +19,49 @@ $rPlayer = mysqli_query($conn, $queryPlayer);
 $queryTeam = "SELECT team.teamID, team.teamPhoto, team.teamName, league.leagueName FROM team
     INNER JOIN  league ON team.teamLeague = league.leagueID";
 $rTeam = mysqli_query($conn, $queryTeam);
+
+$queryGame = "SELECT
+team.teamID,
+team.teamName,
+GROUP_CONCAT(game.GameID) AS GameIDs,
+GROUP_CONCAT(game.gameDate) AS Dates,
+GROUP_CONCAT(team.teamName) AS Equipos
+FROM
+game
+JOIN
+game_team ON game.gameID = game_team.game_id
+JOIN
+team ON team.teamID = game_team.team_id
+GROUP BY
+game.gameID";
+
+$rGame = mysqli_query($conn, $queryGame);
+
+$rowGame = mysqli_fetch_assoc($rGame);
+$rowTeam = mysqli_fetch_assoc($rTeam);
+
+print_r($rowGame);
+
+
+SELECT
+  
+    hola.GameIDs,
+    hola.Dates,
+    SUBSTRING_INDEX(hola.Equipos, ',', 1) AS Equipo1,
+    SUBSTRING_INDEX(hola.Equipos, ',', -1) AS Equipo2
+FROM (
+SELECT
+
+GROUP_CONCAT(DISTINCT game.gameID SEPARATOR ', ') AS GameIDs,
+GROUP_CONCAT(DISTINCT game.gameDate SEPARATOR ', ') AS Dates,
+    
+GROUP_CONCAT(team.teamName) AS Equipos
+FROM
+game
+JOIN
+game_team ON game.gameID = game_team.game_id
+JOIN
+team ON team.teamID = game_team.team_id
+GROUP BY
+game.gameID
+    ) as hola;
